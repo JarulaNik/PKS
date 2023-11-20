95165ed899e42397f22bfba09485da4b033fc888
Jaroslav Zhidkov <jaropolk1104@gmail.com>
2023-11-20 19:17:28 +0300

    Create employee.cs
    Подготовка к XML

---
+++ employee.cs
@@ -0,0 +1,10 @@
+namespace EmployeeManagement
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public string Department { get; set; }
+    }
+}
